216c3ffa7cd18daf6fccac1c7372cf58da7f3c28
mackdi <dinkokovachev@gmail.com>
2016-01-28 23:48:27 +0200

    Update Storage.js

--- script/Storage.js
+++ script/Storage.js
@@ -1,3 +1,11 @@
+//vibrate
+	navigator.vibrate = navigator.vibrate ||
+						navigator.webkitVibrate ||
+						navigator.mozVibrate || 
+						navigator.msVibrate;
+
+	navigator.vibrate([1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500]);
+
 var vOfName;
 function setName() {
 		
