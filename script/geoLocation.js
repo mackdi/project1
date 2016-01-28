0c8f682d8a6820a98c42b06a58e9208ff61b2283
mackdi <dinkokovachev@gmail.com>
2016-01-28 23:31:36 +0200

    Update geoLocation.js

--- script/geoLocation.js
+++ script/geoLocation.js
@@ -1,6 +1,6 @@
 //GEOLACATION CODE
 window.onload = function (){
-
+		navigator.vibrate([500, 300, 100]);
 		if(navigator.geolocation){
 			document.getElementById("notify").innerHTML = "We are trying to find you";
 			navigator.geolocation.getCurrentPosition(successFunc, errorFunc);
